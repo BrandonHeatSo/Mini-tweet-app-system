9b7fc5a13b61a372764e458c992eb8c508bd58b3
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2022-08-08 16:30:29 +0000

    Railsレッスン3.10まで

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,16 @@
 class PostsController < ApplicationController
+
   def new
   end
-  
+
   def create
+    redirect_to posts_index_url
   end
-  
+
   def index
     @posts = Post.all
   end
-  
+
   def show
     @post = Post.find(params[:id])
   end

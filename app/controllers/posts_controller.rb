4f1cf9e19d45c653808f4db2543628823d0b1421
Brandon Heat <stardustgrave100msn@ybb.ne.jp>
2022-08-09 17:46:53 +0000

    Railsレッスン5.7まで（5章が完了！)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,19 @@
 class PostsController < ApplicationController
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(content: params[:content])
-    @post.save
-    redirect_to posts_index_url
+    if @post.save
+      flash[:notice] = "投稿しました！"
+      redirect_to posts_index_url
+    else
+      render :new
+    end
   end
-
+  
   def index
     @posts = Post.all.order(created_at: :desc)
   end
@@ -35,6 +40,7 @@ class PostsController < ApplicationController
   def destroy
     @post = Post.find(params[:id])
     @post.destroy
+    flash[:notice] = "投稿を削除しました。"
     redirect_to posts_index_url
   end
   
